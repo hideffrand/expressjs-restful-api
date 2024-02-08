d75d94036ce05e3baad0363bf1becfc9dd20cbb6
hideffrand <hideffrand@gmail.com>
2024-02-09 01:57:09 +0700

    test deploy on vercel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('server running on port 3000!'))
+app.listen(process.env.SERVER_PORT, () => console.log('server running!'))
